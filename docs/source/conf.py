4e4c3751b88c6292a7819200e31a357bf5a1b165
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-12-03 02:06:33 -0500

    Set up parameters to the document template.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -126,9 +126,6 @@ todo_include_todos = False
 # To add GitHub link.
 rst_prolog = """
 :github_url: https://github.com/rgmining/common
-:base_url: https://rgmining.github.io/common/
-:description: This package provides functions and classes used in
-    Review Graph Mining project.
 """
 
 # -- Options for HTML output ----------------------------------------------
@@ -137,6 +134,16 @@ rst_prolog = """
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+
+html_context = {
+    "base_url": "https://rgmining.github.io/common/",
+    "twitter": "@junkawamoto",
+    "google_analytics": "UA-82315630-2",
+    "google_adsense": "ca-pub-4734862314145555",
+    "google_adsense_slot": "6934003829",
+    "addthis": "ra-57f1fa5b740feddd"
+}
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
